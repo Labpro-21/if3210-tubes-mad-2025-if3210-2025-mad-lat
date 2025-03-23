91263891f91771ff7b4550cd36710e6b4c776633
nerggg <mas.ikhwanalhakim@gmail.com>
2025-03-23 11:33:41 +0700

    project restructuring

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,8 +1,24 @@
 pluginManagement {
     repositories {
+        google {
+            content {
+                includeGroupByRegex("com\\.android.*")
+                includeGroupByRegex("com\\.google.*")
+                includeGroupByRegex("androidx.*")
+            }
+        }
         mavenCentral()
         gradlePluginPortal()
     }
 }
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
 
-rootProject.name = "KotlinNativeTemplate"
+rootProject.name = "Purrytify"
+include(":app")
+ 
