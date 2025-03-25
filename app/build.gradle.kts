53d5b9ec55bd31031812aed5f398daffbf80cd15
nerggg <mas.ikhwanalhakim@gmail.com>
2025-03-25 11:09:26 +0700

    feat: change the SDKs version to match the task specification

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -6,12 +6,12 @@ plugins {
 
 android {
     namespace = "com.tubesmobile.purrytify"
-    compileSdk = 35
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "com.tubesmobile.purrytify"
-        minSdk = 24
-        targetSdk = 35
+        minSdk = 29
+        targetSdk = 34
         versionCode = 1
         versionName = "1.0"
 
